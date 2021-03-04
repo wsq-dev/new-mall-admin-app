2e4a5f7be5bf24401c62855fbb9820d56e66e63c
qi <1002428712@qq.com>
2021-03-04 19:02:41 +0800

    登录校验

---
+++ src/components/sliderNav.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="silder-nav">
+    <a-button type="primary" @click="toggleCollapsed">
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <div class="breadcrumb">
+      <a-breadcrumb>
+        <a-breadcrumb-item>商品</a-breadcrumb-item>
+        <a-breadcrumb-item>
+          <a href>商品列表</a>
+        </a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <ul class="user-info">
+      <li class="user-name">
+        {{$store.state.user.username}}
+        <a-icon type="down" />
+      </li>
+      <li class="login-out" @click="logout">退出</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+    logout() {
+      this.$store.dispatch('logout');
+      this.$router.push({
+        name: 'Login',
+      });
+    },
+  },
+};
+</script>
